1b3503afe65086cf0acfc3ed67370f7259665a02
kaser137 <murilon375@gmail.com>
2024-04-10 18:48:51 +0300

    add

--- sasha/settings.py
+++ sasha/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         'NAME': 'sb',
         'USER': 'sbu',
         'PASSWORD': 'sbp',
-        'HOST': ' localhost',
+        'HOST': ' http://0.0.0.0',
         'PORT': '5432',
     },
     # 'default': dj_database_url.config(
