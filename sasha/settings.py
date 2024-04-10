f806d21dab4868ead53466a3377ffc66c1cd1e20
kaser137 <murilon375@gmail.com>
2024-04-10 18:20:44 +0300

    add

--- sasha/settings.py
+++ sasha/settings.py
@@ -80,12 +80,20 @@ WSGI_APPLICATION = 'sasha.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'old_db': dj_database_url.config(
-        default='sqlite:////{0}'.format(os.path.join(BASE_DIR, 'db.sqlite3'))
-    ),
-    'default': dj_database_url.config(
-        default=os.getenv('DB_URL')
-    )
+    # 'old_db': dj_database_url.config(
+    #     default='sqlite:////{0}'.format(os.path.join(BASE_DIR, 'db.sqlite3'))
+    # ),
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'sb',
+        'USER': 'sbu',
+        'PASSWORD': 'sbp',
+        'HOST': 'localhost',
+        'PORT': '',
+    },
+    # 'default': dj_database_url.config(
+    #     default=os.getenv('DB_URL')
+    # )
     # 'default': {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': BASE_DIR / 'db.sqlite3',
