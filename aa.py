8a66df8ae0a30535ec81cd6570099b44ec851922
kaser137 <murilon375@gmail.com>
2024-04-11 07:02:01 +0300

    add

--- aa.py
+++ aa.py
@@ -1,10 +1,10 @@
-import os
-from dotenv import load_dotenv
-import time
+# import os
+# from dotenv import load_dotenv
+# import time
 
-load_dotenv()
+# load_dotenv()
 print("How do you do, my Sweetheart?! I miss you!")
-#time.sleep(720)
+# time.sleep(720)
 print("qqq5555555")
-a = os.getenv('app_name')
-print(a)
+# a = os.getenv('app_name')
+# print(a)
