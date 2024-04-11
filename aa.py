618d15becf88f841ca6083211f8101128c4d72f8
kaser137 <murilon375@gmail.com>
2024-04-12 02:02:39 +0300

    add

--- aa.py
+++ aa.py
@@ -3,7 +3,7 @@
 # import time
 
 # load_dotenv()
-print("How do you do, my Sweetheart?! I miss you!")
+print("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
 # time.sleep(720)
 print("qqq5555555")
 # a = os.getenv('app_name')
