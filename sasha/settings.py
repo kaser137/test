b1df6b1292875234e471f8ce52f6bb93979dea17
kaser137 <murilon375@gmail.com>
2024-04-14 17:56:41 +0300

    add

--- sasha/settings.py
+++ sasha/settings.py
@@ -138,6 +138,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = 'media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
